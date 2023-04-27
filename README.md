2ca660b979b8df5162239b1de5e27673a8098f72
Ezpie <ezpie.co@gmail.com>
2023-04-27 16:11:25 +0530

    init repository

---
+++ README.md
@@ -0,0 +1,5 @@
+# CrazyCss Documentation
+
+This is the repository of the official documentation site for CrazyCss. A library to create UI components.
+
+Visits the site [here](https://crazy.vercel.app)
---
+++ docusaurus.config.js
@@ -0,0 +1,122 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "CrazyCss",
+  tagline: "The craziest css library to create UI components",
+  favicon: "img/favicon.svg",
+
+  // Set the production url of your site here
+  url: "https://crazycss.vercel.app",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // NOTE: May add later
+          // editUrl: "https://github.com/ezpieco/crazycss-docs",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/default.svg",
+      navbar: {
+        title: "CrazyCss",
+        logo: {
+          alt: "Ezpie Logo",
+          src: "img/favicon.svg",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "right",
+            label: "Tutorial",
+          },
+          {
+            href: "https://github.com/ezpie/crazycss",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Discord",
+                href: "https://discord.gg/VXbjU5KDza",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/ezpieco",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/ezpieco/crazycss",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} CrazyCss. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
