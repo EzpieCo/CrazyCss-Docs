886c5252f54a2ffd2ee72838447a47123c56e11c
Ezpie <ezpie.co@gmail.com>
2023-05-03 19:08:55 +0530

    Fixed and checked. Ready for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -100,10 +100,6 @@ const config = {
             title: "More",
             items: [
               {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
                 label: "GitHub",
                 href: "https://github.com/ezpieco/crazycss",
               },
